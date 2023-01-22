27ddb2fa9d2bec004f5b627b130d867eaa504e77
Diestro <eldiestro171@gmail.com>
2023-01-22 21:29:27 +0100

    Update GetPostQueryHandler.cs

--- src/CQRS-MediatR-NET-6/Handlers/Queries/GetPostQueryHandler.cs
+++ src/CQRS-MediatR-NET-6/Handlers/Queries/GetPostQueryHandler.cs
@@ -13,7 +13,7 @@ public class GetPostQueryHandler : IRequestHandler<GetPostRequestModel, GetPostR
         return new GetPostResponseModel()
         {
             Body = "It is a long established fact that a reader will...",
-            Id = Guid.Parse("556b8afa-5617-425c-87af-381f278ccf91"),
+            Id = Guid.Parse("556b8afa-5617-425c-87af-381f278ccf90"),
             Title = "What is Lorem Ipsum?",
             UserId = Guid.Parse("40cea88f-9cdb-4509-a8bd-a307d149daf0")
         };
